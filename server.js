15a7ddce96be3453bb72134fd70fbab9e2e53f06
Curt Schumacher <schumacr@mail.uc.edu>
2014-08-04 19:04:39 -0400

    add logging for all requests to the console - might get annoying - we will see

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ var express = require('express'),
 app = express(),
 mongoose = require('mongoose'),
 bodyParser = require('body-parser'),
-methodOverride = require('method-override');
+methodOverride = require('method-override')
+morgan = require('morgan');
 
 var db = require('./config/db');
 
 mongoose.connect(db.url);
 
 var port = process.env.PORT || 3010;
+
+app.use(morgan('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
