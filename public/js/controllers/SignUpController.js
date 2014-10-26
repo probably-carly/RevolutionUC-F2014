c027bce1b3576d5b89173a3b4693832e75913b2f
Curt Schumacher <schumacr@mail.uc.edu>
2014-10-26 00:45:17 -0400

    Add hosted by UCIT
    Add UCIT preferred logo and Strap's logo

--- public/js/controllers/SignUpController.js
+++ public/js/controllers/SignUpController.js
@@ -1,36 +1,37 @@
 app.controller('SignUpCtrl', function ($scope, $modal, $log) {
-    $scope.open = function () {
-        var modalInstance = $modal.open({
-            templateUrl: 'views/signup.html',
-            controller: 'SignUpInstanceCtrl',
-            size: 'lg'
-        });
-    };
-})
-    .controller('SignUpInstanceCtrl', function ($scope, $http, $window) {
-        $scope.user = {};
-
-        $scope.forgetIt = function () {
-            $scope.$dismiss();
-            toastr.error("Forgotten!");
-        };
-
-        $scope.submit = function () {
-            $http.post('/api/newuser', $scope.user).success(function (responseData) {
-                if (responseData.err) {
-                    toastr.error("It looks like you've signed up already!", "Oh Dear!");
-                    console.log("User already signed up!");
-                } else {
-                    $scope.$dismiss();
-                    toastr.success("We'll see you there!");
-                }
-            });
-        };
-
-
-        $scope.uploadResume = function () {
-            $window.filepicker.pick(function (uploadedFile) {
-                $scope.$apply($scope.user.resume = uploadedFile.filename);
-            });
-        }
+  $scope.open = function () {
+    var modalInstance = $modal.open({
+      templateUrl: 'views/signup.html',
+      controller: 'SignUpInstanceCtrl',
+      size: 'lg'
     });
+  };
+})
+  .controller('SignUpInstanceCtrl', function ($scope, $http, $window) {
+    $scope.user = {};
+
+    $scope.forgetIt = function () {
+      $scope.$dismiss();
+      toastr.error("Forgotten!");
+    };
+
+    $scope.submit = function () {
+      $http.post('/api/newuser', $scope.user).success(function (responseData) {
+        if (responseData.err) {
+          toastr.error("It looks like you've signed up already!", "Oh Dear!");
+          console.log("User already signed up!");
+        } else {
+          $scope.$dismiss();
+          toastr.success("We'll see you there!");
+        }
+      });
+    };
+
+
+    $scope.uploadResume = function () {
+      $window.filepicker.pick(function (uploadedFile) {
+        $scope.$apply($scope.user.resume = uploadedFile.filename);
+      });
+    }
+  });
+
