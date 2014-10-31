128cda68522b268bd300e5ea9755888b85ce52be
Curt Schumacher <schumacr@mail.uc.edu>
2014-10-31 00:22:02 -0400

    reformat sponsor logos
    Add intuit, and CBTS logos reformat for sponsor tiers

--- app/routes.js
+++ app/routes.js
@@ -1,22 +1,22 @@
 module.exports = function (app, mongoose) {
-    var Hacker = mongoose.model('Hacker', require('./models/hacker.js'));
-    var fs = require('fs');
+  var Hacker = mongoose.model('Hacker', require('./models/hacker.js'));
+  var fs = require('fs');
 
-    app.post('/api/newuser', function (req, res) {
-        Hacker.create(req.body, function (err) {
-            if (err) {
-                res.send(err);
-                return;
-            }
-            res.send({"response": "200"});
-        });
+  app.post('/api/newuser', function (req, res) {
+    Hacker.create(req.body, function (err) {
+      if (err) {
+        res.send(err);
+        return;
+      }
+      res.send({"response": "200"});
     });
+  });
 
-    app.get('/api/hackercount', function (req, res) {
-        Hacker.count(function (err, count) {
-            if (!err) {
-                res.json(count);
-            }
-        });
+  app.get('/api/hackercount', function (req, res) {
+    Hacker.count(function (err, count) {
+      if (!err) {
+        res.json(count);
+      }
     });
+  });
 };
